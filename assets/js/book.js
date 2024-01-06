1d7d2b87531d92cf99e0cdf028effe1456d93e7a
Jason <king.gs1314@outlook.com>
2024-01-06 11:27:49 -0500

    complete page transfer function and qr code demo

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,3 +1,8 @@
-const url_params = new URLSearchParams(window.location.search)
+let test = document.querySelector("#book-name")
 
-let test = $("#test")
+// get data from main page
+const url_params = (new URLSearchParams(window.location.search)).get('book_name');
+
+test.textContent = url_params;
+
+// need to use book api here
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 let qr_code_element = $(".qr-code")
-let info = ``
-const url = `https://quickchart.io/qr?text=${encodeURIComponent(info)}&centerImageUrl=https://assetstorev1-prd-cdn.unity3d.com/key-image/9c3bffdf-a21b-4e08-94d5-63d5bbc3528b.jpg`;
-const options = {
-	method: 'GET'
+let page_transfer_btn = $(".page-transfer-btn")
 
-};
+transfer_page = function(book_name){
+	window.location.href = `book.html?book_name=${encodeURIComponent(book_name)}`
+}
 
-fetch(url, options)
-	.then(response => response)
-	.then(response => qr_code_element.attr("src", response.url))
+
+
+qr_code_generator = function(info){
+	
+	const url = `https://quickchart.io/qr?text=${encodeURIComponent(info)}&centerImageUrl=https://images.squarespace-cdn.com/content/v1/598a797af5e23155afc4d592/1597998089824-UHZER996H8NB5EYYDFIW/AVI.JPG`;
+	const options = {
+		method: 'GET'
+
+	};
+
+	fetch(url, options)
+		.then(response => response)
+		.then(response => qr_code_element.attr("src", response.url))
+
+}
+
+qr_code_generator("https://github.com/Jason897571/Magical-Book-Browser")
+page_transfer_btn.on("click", function(){transfer_page("Magical Book")})
 	
