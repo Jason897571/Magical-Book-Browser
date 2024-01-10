40aede0aa26e0df96396bf5e9b496385a218f9fd
Jason <king.gs1314@outlook.com>
2024-01-10 13:12:27 -0500

    complete the transfer page function

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,11 +1,35 @@
+const modal_sign_in_element = $(".sign-in-modal")
+const modal_sign_in_close_element = $(".sign-in-close")
+const nav_sign_in_element = $("#nav-sign-in")
+
+
 let book_image = document.querySelector(".book-image")
 let book_name = document.querySelector(".book-name")
 let book_author = document.querySelector(".book-author")
-let back_btn = document.querySelector(".back-btn")
+
 
 // get data from main page
 const url_params = (new URLSearchParams(window.location.search)).get('input_value');
 
+
+// open sign in modal
+open_sign_in_modal = function(){
+	modal_sign_in_element.addClass("is-active")
+}
+//close sign modal
+close_sign_in_modal = function(){
+	modal_sign_in_element.removeClass("is-active")
+}
+
+// add event to open the sign in modal
+nav_sign_in_element.on("click",function(){
+	open_sign_in_modal()
+})
+
+//add event to close the sign in modal
+modal_sign_in_close_element.on("click",function(){
+	close_sign_in_modal()
+})
 
 
 // add book name,image, author,catagory below
@@ -18,6 +42,6 @@ show_book = function(image,name,author,catagory){
 }
 
 
-back_btn.on("click", function(){window.history.back()})
+
 
 // need to use book api here
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,7 @@ const modal_sign_in_close_element = $(".sign-in-close")
 const nav_sign_in_element = $("#nav-sign-in")
 const search_btn = $("#search-btn")
 const search_input_box = $("#search-box")
-const search_catagory_element = $("#search-category")
-
+const search_catagory_element = $("#search-category option:selected")
 
 
 // open sign in modal
@@ -31,27 +30,29 @@ modal_sign_in_close_element.on("click",function(){
 
 transfer_page = function(){
 	let input_value = search_input_box.val()
-	search_catagory_element.change(function(){
-		let catagory_value = $(this).val()
-	})
+	let category_value = search_catagory_element.val()
 
-	alert(input_value + " " + catagory_value)
+	alert(category_value)
+	
 
 	if(input_value == ""){
 		//TODO need change to modal
 		alert("please input something")
 	}else{
 		// transfer to another page to show result
-		window.location.href = `book.html`
+		window.location.href = `book.html?input=${encodeURIComponent(input_value)}&category=${encodeURIComponent(category_value)}`
 	}
 	
 }
 
-page_transfer_btn.on("click", function(){transfer_page()})
+page_transfer_btn.on("click", function(event){
+	event.preventDefault();
+	transfer_page()
+})
 
 qr_code_generator = function(info){
 	
-	const url = `https://quickchart.io/qr?text=${encodeURIComponent( info)}&centerImageUrl=https://images.squarespace-cdn.com/content/v1/598a797af5e23155afc4d592/1597998089824-UHZER996H8NB5EYYDFIW/AVI.JPG`;
+	const url = `https://quickchart.io/qr?text=${encodeURIComponent(info)}&centerImageUrl=https://images.squarespace-cdn.com/content/v1/598a797af5e23155afc4d592/1597998089824-UHZER996H8NB5EYYDFIW/AVI.JPG`;
 	const options = {
 		method: 'GET'
          
