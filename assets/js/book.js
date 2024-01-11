87c3205515875d263208f2030d8e59448e0ad0dc
Jason <king.gs1314@outlook.com>
2024-01-11 15:16:08 -0500

    complete the card display in page 2

--- assets/js/book.js
+++ assets/js/book.js
@@ -6,7 +6,7 @@ const nav_sign_in_element = $("#nav-sign-in")
 let book_image = document.querySelector(".book-image")
 let book_name = document.querySelector(".book-name")
 let book_author = document.querySelector(".book-author")
-let result_container= document.querySelector(".result-container")
+let result_container= $(".result-container")
 
 // get data from main page
 const input_value = (new URLSearchParams(window.location.search)).get('input');
@@ -37,13 +37,13 @@ modal_sign_in_close_element.on("click",function(){
 // need to use book api here
 run_book_api = function(user_input,category_type){
     // use the api here
-    if(category_type=="author"){
+    if(category_type=="inauthor"){
       var bookApi = `https://www.googleapis.com/books/v1/volumes?q=${user_input}+inauthor&maxResults=40`
     }
-    else if(category_type=="book-name"){
+    else if(category_type=="intitle"){
       var bookApi = `https://www.googleapis.com/books/v1/volumes?q=${user_input}+intitle&maxResults=40`
     }
-    else if(category_type=="genre"){
+    else if(category_type=="subject"){
       var bookApi = `https://www.googleapis.com/books/v1/volumes?q=${user_input}+subject&maxResults=40`
     }
 
@@ -75,19 +75,50 @@ function handleResponse(response) {
     }
 }
 function createBookCard(bookInfo) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const image = document.createElement('img');
-    image.src = bookInfo.imageLinks;
-    card.appendChild(image);
-    const title = document.createElement('div');
-    title.classList.add('book-title');
-    title.textContent = bookInfo.title;
-    card.appendChild(title);
-    const author = document.createElement('div');
-    author.classList.add('author');
-    author.textContent = bookInfo.authors;
-    card.appendChild(author);
-    result_container.appendChild(card);
+  // create card holder
+  const card_column = $("<div></div>")
+  card_column.addClass("card-column")
+  result_container.append(card_column)
+  // create card figure
+  const figure = $("<figure></figure>")
+  figure.addClass("card code-card")
+  card_column.append(figure)
+  // create card name
+  const card_name = $("<h2></h2>")
+  card_name.addClass("card-header")
+  card_name.text(bookInfo.title)
+  figure.append(card_name)
+  // create card pre section
+  const card_pre = $("<pre></pre>")
+  card_pre.addClass("code-block")
+  figure.append(card_pre)
+  //create card image
+  const card_image = $("<img></img>")
+  card_image.attr("src", bookInfo.imageLinks)
+  card_image.attr("alt", "book logo")
+  card_pre.append(card_image)
+  //create author info
+  const card_author = $("<p></p>")
+  card_author.text("Author: " + bookInfo.authors)
+  card_author.addClass("card-author")
+  card_pre.append(card_author)
+
+
+
+
+ /*  const card = document.createElement('div');
+  card.classList.add('card');
+  const image = document.createElement('img');
+  image.src = bookInfo.imageLinks;
+  card.appendChild(image);
+  const title = document.createElement('div');
+  title.classList.add('book-title');
+  title.textContent = bookInfo.title;
+  card.appendChild(title);
+  const author = document.createElement('div');
+  author.classList.add('author');
+  author.textContent = bookInfo.authors;
+  card.appendChild(author);
+  result_container.appendChild(card); */
 }
 run_book_api(input_value,category_type)
